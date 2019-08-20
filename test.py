815f17d5040bb154563e644245650ed5b67d08ed
LianLuo <392193761@qq.com>
2019-08-20 13:58:57 +0800

    add next method for program

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-#打印99乘法表
+#不同方法打印99乘法表
 # -*- coding: utf-8 -*-
 for i in range(1,10):
     print (i-1)*("       "),
@@ -6,3 +6,15 @@ for i in range(1,10):
         line='{}*{}={:<2}'.format(j,i,j*i)
         print line,
     print ""
+
+
+for i in range(1,10):
+    for j in range(1, 10):
+        if i>j:
+            line= '{} {} {:<2}'.format(" "," "," ")
+        else:
+            line = '{}*{}={:<2}'.format(i, j, i * j)
+        print line,
+    print ""	
+
+
